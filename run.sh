1ea032bac1c6bbe33dce418351b0611c85ac1f86
Florian Scheidl <florian.scheidl@gmail.com>
2023-03-27 18:58:46 +0200

    Run file for slurm batch

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="job_name"
 #SBATCH --account="g34"
 #SBATCH --time=01:00:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
@@ -17,4 +17,4 @@ export NCCL_IB_HCA=ipogif0
 export NCCL_IB_CUDA_SUPPORT=1
 export NCCL_SOCKET_IFNAME=ipogif0
 
-srun python gcn_distr_15d.py
+srun python gcn_distr_15d.py --dist_file="dist_file_x"
