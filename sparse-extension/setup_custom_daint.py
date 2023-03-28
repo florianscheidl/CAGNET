86f0a29be2eb016d10c3e99a1723634e0f7e2776
Florian Scheidl <florian.scheidl@gmail.com>
2023-03-28 11:17:32 +0200

    Daint setup debugging

--- sparse-extension/setup_custom_daint.py
+++ sparse-extension/setup_custom_daint.py
@@ -6,5 +6,5 @@ setup(name='sparse_coo_tensor_cpp',
                                               extra_compile_args=["-lcusparse"],
                                               include_dirs=["/opt/nvidia/cudatoolkit10.2/10.2.89_3.28-2.1__g52c0314/include",
                                                             "/opt/nvidia/cudatoolkit10.2/10.2.89_3.28-2.1__g52c0314/lib64"],
-                                              extra_link_args=["-lcusparse_static", "-lcudart_static", "-lpthread", "-ldl"],)],
+                                              extra_link_args=["-lpthread", "-ldl"],)],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
