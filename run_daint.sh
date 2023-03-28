ee51074a3bb4831c75ef7d8d1178ee26b645c3ad
Florian Scheidl <florian.scheidl@gmail.com>
2023-03-28 18:11:02 +0200

    New batch file

---
+++ run_daint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name="job_name"
+#SBATCH --account="g34"
+#SBATCH --time=30:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=gpu
+#SBATCH --partition=debug
+#SBATCH --mem=50G
+#SBATCH --hint=nomultithread
+
+module load cudatoolkit/10.2.89_3.28-2.1__g52c0314
+conda activate cag3
+
+export OMPI_COMM_WORLD_SIZE=$((SLURM_NTASKS * SLURM_NTASKS_PER_NODE))
+export NCCL_DEBUG=INFO
+export NCCL_IB_HCA=ipogif0
+export NCCL_IB_CUDA_SUPPORT=1
+export NCCL_SOCKET_IFNAME=ipogif0
+
+srun python3.6 gcn_distr_15d.py --dist_file="dist_file_x"
