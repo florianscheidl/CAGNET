fbc98e2f9fab6509165f7f47329a818fd6d52dbd
Florian Scheidl <florian.scheidl@gmail.com>
2023-03-28 11:34:20 +0200

    Daint setup debugging

--- sparse-extension/setup_custom_daint.py
+++ sparse-extension/setup_custom_daint.py
@@ -3,8 +3,8 @@ from torch.utils import cpp_extension
 
 setup(name='sparse_coo_tensor_cpp',
       ext_modules=[cpp_extension.CppExtension('sparse_coo_tensor_cpp', ['sparse_coo_tensor.cpp'],
-                                              extra_compile_args=["-lcusparse", "-lcusparse_static", "-lcudart_static"],
+                                              extra_compile_args=["-lcusparse"],
                                               include_dirs=["/opt/nvidia/cudatoolkit10.2/10.2.89_3.28-2.1__g52c0314/include",
                                                             "/opt/nvidia/cudatoolkit10.2/10.2.89_3.28-2.1__g52c0314/lib64"],
-                                              extra_link_args=["-lpthread", "-ldl"],)],
+                                              extra_link_args=["-lcusparse_static", "-lcudart_static", "-lpthread", "-ldl"])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
